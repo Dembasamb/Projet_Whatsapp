93208644d0aa9424195afa6b695f1293be81f5c3
Demba Samb <dembasamb012@gmail.com>
2023-07-17 12:09:36 +0000

    changement effectuer

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,176 +1,186 @@
-/********************APPELLE D'ELLEMENTS******************************/
 body=document.querySelector('body')
-body.style.backgroundColor='rgb(11, 11, 23)'
-/**************************************************/
-header_div=document.createElement('div')
-man_div=document.createElement('div')
-body.appendChild(header_div)
-body.appendChild(man_div)
 body.style.display='flex'
-body.style.flexDirection='column'
-/**************************************************/
-head1_div=document.createElement('div')
-head2_div=document.createElement('div')
-header_div.appendChild(head1_div)
-header_div.appendChild(head2_div)
-header_div.style.display='flex'
-header_div.style.flexDirection='column'
-/**************************************************/
-head11_div=document.createElement('div')
-head12_div=document.createElement('div')
-head13_div=document.createElement('div')
-head14_div=document.createElement('div')
-head1_div.appendChild(head11_div)
-head1_div.appendChild(head12_div)
-head1_div.appendChild(head13_div)
-head1_div.appendChild(head14_div)
-head1_div.style.display='flex'
-/**************************************************/
-head21_div=document.createElement('div')
-head22_div=document.createElement('div')
-head23_div=document.createElement('div')
-head2_div.appendChild(head21_div)
-head2_div.appendChild(head22_div)
-head2_div.appendChild(head23_div)
-head2_div.style.display='flex'
-/**************************************************/
-head11_div.style.width='70%'
-head12_div.style.width='10%'
-head13_div.style.width='10%'
-head14_div.style.width='10%'
-head1_div.style.height='20vh'
-head1_div.style.width='100%'
-head1_div.style.border='2px solid'
-head11_div.style.border='1.5px solid blue'
-head12_div.style.border='1.5px solid blue'
-head13_div.style.border='1.5px solid blue'
-head14_div.style.border='1.5px solid blue'
-/**************************************************/
-head21_div.style.width='33%'
-head22_div.style.width='33%'
-head23_div.style.width='33%'
-head2_div.style.height='10vh'
-head2_div.style.width='100%'
-head2_div.style.border='2px solid grey'
-head21_div.style.border='1.5px solid red'
-head22_div.style.border='1.5px solid red'
-head23_div.style.border='1.5px solid red'
-/**************************************************/
-man_div.style.border='2px solid green'
+body.style.backgroundColor='rgb(13, 11, 27)'
+/*******************************************************/
+man_div=document.createElement('div')
+man_div.style.display='flex'
+man_div.style.flexDirection='column'
+man_div.style.border='2px solid blue'
 man_div.style.width='100%'
-man_div.style.height='70vh'
-/**************************************************/
+man_div.style.height='97vh'
+body.appendChild(man_div)
+/*******************************************************/
 man1_div=document.createElement('div')
-man2_div=document.createElement('div')
-man_div.appendChild(man1_div)
-man_div.appendChild(man2_div)
 man1_div.style.display='flex'
-man2_div.style.display='flex'
+man1_div.style.flexDirection='column'
+man1_div.style.border='1.5px solid red'
+man1_div.style.width='100%'
+man1_div.style.height='87vh'
+man_div.appendChild(man1_div)
+/********************************************************/
+man2_div=document.createElement('div')
+man2_div.style.border='1.5px solid green'
+man2_div.style.width='6vw'
+man2_div.style.height='13vh'
+man2_div.style.marginLeft='77rem'
+man_div.appendChild(man2_div)
+/******************************************************/
 man11_div=document.createElement('div')
-man12_div=document.createElement('div')
 man11_div.style.display='flex'
-man12_div.style.display='flex'
-man12_div.style.flexDirection='column'
 man11_div.style.border='1.5px solid grey'
-man11_div.style.width='15vw'
-man11_div.style.height='15vh'
-man11_div.style.borderRadius='50%'
-man11_div.style.backgroundColor='rgb(0, 0, 0)'
-man11_div.style.marginTop='0.4em'
-man12_div.style.border='1.5px solid yellow'
-man12_div.style.width='80vw'
-man12_div.style.height='20vh'
+man11_div.style.width='100%'
+man11_div.style.height='20vh'
 man1_div.appendChild(man11_div)
+/*******************************************************/
+man111_div=document.createElement('div')
+man111_div.style.display='flex'
+man111_div.style.border='1.5px solid red'
+man111_div.style.width='6vw'
+man111_div.style.height='12vh'
+man111_div.style.borderRadius='50%'
+man11_div.appendChild(man111_div)
+/**********************PHOTO PROFILE*********************************/
+photos_p_1=document.createElement('img')
+photos_p_1.setAttribute('src','Photos/Logo.png')
+photos_p_1.style.width='6vw'
+photos_p_1.style.height='12vh'
+photos_p_1.style.borderRadius='50%'
+man111_div.appendChild(photos_p_1)
+/*******************************************************/
+/*******************************************************/
+/*******************************************************/
+man112_div=document.createElement('div')
+man112_div.style.display='flex'
+man112_div.style.flexDirection='column'
+man112_div.style.border='1.5px solid blue'
+man112_div.style.width='84vw'
+man112_div.style.height='20vh'
+man11_div.appendChild(man112_div)
+/*******************************************************/
+name1_div=document.createElement('div')
+name1_div.style.border='1.5px solid green'
+name1_div.style.width='84vw'
+name1_div.style.height='10vh'
+man112_div.appendChild(name1_div)
+/**********************USER NAME*********************************/
+name1=document.createElement('H2')
+name1.textContent='User Name'
+name1.style.color='white'
+name1_div.appendChild(name1)
+name1.style.marginLeft='2em'
+/*******************************************************/
+message1_div=document.createElement('div')
+message1_div.style.border='1.5px solid green'
+message1_div.style.width='84vw'
+message1_div.style.height='10vh'
+man112_div.appendChild(message1_div)
+/**********************USER MESSAGE*********************************/
+message1=document.createElement('H3')
+message1.textContent='nouveau messages'
+message1.style.color='white'
+message1_div.appendChild(message1)
+message1.style.marginLeft='1.5em'
+/*******************************************************/
+/*******************************************************/
+/*******************************************************/
+man113_div=document.createElement('div')
+man113_div.style.display='flex'
+man113_div.style.flexDirection='column'
+man113_div.style.border='1.5px solid red'
+man113_div.style.width='7vw'
+man113_div.style.height='20vh'
+man11_div.appendChild(man113_div)
+/*******************************************************/
+time1_div=document.createElement('div')
+time1_div.style.border='1.5px solid green'
+time1_div.style.width='7vw'
+time1_div.style.height='10vh'
+man113_div.appendChild(time1_div)
+/***********************MESSAGE TIMES********************************/
+time1=document.createElement('H3')
+time1.textContent='22h:07'
+time1.style.color='white'
+time1_div.appendChild(time1)
+time1.style.marginLeft='1em'
+/*******************************************************/
+number1_div=document.createElement('div')
+number1_div.style.border='1.5px solid green'
+number1_div.style.width='7vw'
+number1_div.style.height='10vh'
+man113_div.appendChild(number1_div)
+/**********************MESSAGES NUMBER*********************************/
+number1=document.createElement('H3')
+number1.textContent='2'
+number1.style.color='green'
+number1_div.appendChild(number1)
+number1.style.marginLeft='2em'
+/*******************************************************/
+/*******************************************************/
+/*******************************************************/
+man12_div=document.createElement('div')
+man12_div.style.display='flex'
+man12_div.style.border='1.5px solid grey'
+man12_div.style.width='100%'
+man12_div.style.height='20vh'
 man1_div.appendChild(man12_div)
-man121_div=document.createElement('div')
-man121_div.style.border='1.5px solid blue'
-man121_div.style.width='100%'
-man121_div.style.height='10vh'
-man121_div.style.display='flex'
-man122_div=document.createElement('div')
-man122_div.style.border='1.5px solid RED'
-man122_div.style.width='100%'
-man122_div.style.height='10vh'
-man1211_div=document.createElement('div')
-man1212_div=document.createElement('div')
-man12_div.appendChild(man121_div)
-man12_div.appendChild(man122_div)
-man121_div.appendChild(man1211_div)
-man121_div.appendChild(man1212_div)
-man1211_div.style.border='1.5px solid blue'
-man1211_div.style.width='80%'
-man1211_div.style.height='10vh'
-man1212_div.style.border='1.5px solid blue'
-man1212_div.style.width='20%'
-man1212_div.style.height='10vh'
-/**************************************************/
+/*******************************************************/
 
-/**************************************************/
-man2_div.style.border='2px solid blue'
-man2_div.style.width='10%'
-man2_div.style.height='10vh'
-man2_div.style.marginLeft='33em'
-man2_div.style.marginTop='15em'
-/**************************************************/
-image=document.createElement('img')
-image.setAttribute('src','Photos/Logo.png')
-image.style.width='15vw'
-image.style.height='15vh'
-image.style.borderRadius='50%'
-man11_div.appendChild(image)
-/**************************************************/
-h1=document.createElement('h1')
-head11_div.appendChild(h1)
-h1.textContent='Whatsaap'
-h1.style.color='lightgrey'
-/**************************************************/
-h3=document.createElement('h3')
-head21_div.appendChild(h3)
-h3.textContent='Discution'
-h3.style.color='lightgrey'
-h3.style.marginLeft='3em'
-h3.style.marginTop='2em'
-/**************************************************/
-h3=document.createElement('h3')
-head22_div.appendChild(h3)
-h3.textContent='Statut'
-h3.style.color='lightgrey'
-h3.style.marginLeft='3em'
-h3.style.marginTop='2em'
-/**************************************************/
-h3=document.createElement('h3')
-head23_div.appendChild(h3)
-h3.textContent='Appels'
-h3.style.color='lightgrey'
-h3.style.marginLeft='3em'
-h3.style.marginTop='2em'
-/**************************************************/
-h3=document.createElement('h3')
-man1211_div.appendChild(h3)
-h3.textContent='Sadio Mane'
-h3.style.color='lightgrey'
-h3.style.marginLeft='1em'
-/**************************************************/
-h5=document.createElement('h5')
-man122_div.appendChild(h5)
-h5.textContent='Stl Demba cc ?'
-h5.style.color='lightgrey'
-h5.style.marginLeft='1em'
-/**************************************************/
-h3=document.createElement('h3')
-man1212_div.appendChild(h3)
-h3.textContent='14:37'
-h3.style.color='lightgrey'
-h3.style.marginLeft='1em'
-/**************************************************/
-h4=document.createElement('h3')
-man1212_div.appendChild(h4)
-h4.textContent='2' 
-h4.style.color='green'
-h4.style.marginLeft='2em'
-h4.style.marginTop='2em'
-/**************************************************/
-/**************************************************/
+/*******************************************************/
+man13_div=document.createElement('div')
+man13_div.style.display='flex'
+man13_div.style.border='1.5px solid grey'
+man13_div.style.width='100%'
+man13_div.style.height='20vh'
+man1_div.appendChild(man13_div)
+/*******************************************************/
+
+/*******************************************************/
+man14_div=document.createElement('div')
+man14_div.style.display='flex'
+man14_div.style.border='1.5px solid grey'
+man14_div.style.width='100%'
+man14_div.style.height='20vh'
+man1_div.appendChild(man14_div)
+/*******************************************************/
+man15_div=document.createElement('div')
+man15_div.style.display='flex'
+man15_div.style.border='1.5px solid grey'
+man15_div.style.width='100%'
+man15_div.style.height='20vh'
+man1_div.appendChild(man15_div)
+/*******************************************************/
+/*******************************************************/
+/*******************************************************/
+/*******************************************************/
+
+
+/*******************************************************/
+/*******************************************************/
+/*******************************************************/
+/*******************************************************/
+
+
+/*******************************************************/
+/*******************************************************/
+/*******************************************************/
+/*******************************************************/
+
+
+/*******************************************************/
+/*******************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
